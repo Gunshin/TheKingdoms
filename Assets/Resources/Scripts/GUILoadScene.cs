d8f956cd30f4368e4dc35481324afbd34d882984
Michael Stephens <michaelstephens1993@gmail.com>
2014-12-02 00:35:37 +0000

    started sorting out the project into seperate scenes so i can have a visual test bed for comparing my A*, and a scene for my AI

---
+++ Assets/Resources/Scripts/GUILoadScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUILoadScene : MonoBehaviour {
+
+    public void LoadScene(string name_)
+    {
+        Application.LoadLevel(name_);
+    }
+
+}
